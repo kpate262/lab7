7b551b521835436eba3195ceb76575fd33b38dd3
root <root@Kishan-XPS-13.localdomain>
2018-10-21 15:09:41 -0500

    first commit

---
+++ lab7.cpp
@@ -0,0 +1,37 @@
+#include "stdio.h"
+#include "iostream"
+
+struct Node{
+  int d;
+  Node *next;
+};
+
+void printL(Node *list){
+  Node *p = list;
+  while(p != NULL){
+    printf("%d\n", p.d);
+    p = p.next;
+  }
+}
+
+
+int main(){
+  int i = 0;
+  Node *list = new Node;
+  list->d = i;
+  list->next = NULL;
+
+  Node *p = list;
+  while(i != 5){
+    i++;
+    Node *temp = new Node;
+    temp->d = i;
+    temp->next = NULL;
+    p.next = temp;
+    p = temp;
+    temp = NULL;
+    delete(temp);
+  }
+  printL(list);
+  return 0;
+}
